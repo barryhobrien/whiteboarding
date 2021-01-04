ace062ac168ac255555edddc93b049ad6279a98b
barryhobrien <60525247+barryhobrien@users.noreply.github.com>
2021-01-03 19:50:01 -0700

    Update stack.py

--- stack.py
+++ stack.py
@@ -1,3 +1,10 @@
+# Implement a stack that has the following methods:
+# 1. push(item), which pushes an element onto the stack
+# 2. pop(), which pops off and returns the topmost element of the stack. If there are no elements in the stack,
+#           then it should throw an error or return null. 
+# 3. max(), which returns the maximum value in the stack currently.
+# Each method should run in constant time.
+
 class Stack:
   def __init__(self):
     self.stack = []
